05f21c69877608bfd1bcd923a931aeb25ab07dce
MJ20027 <mi02code@gmail.com>
2023-07-09 23:53:58 +0530

    seventh commit

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'bjdbhbdbdvbdusfdjkl';
 
-app.use(cors({credentials:true}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
